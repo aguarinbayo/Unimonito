7a71b74b0069ab56c439c7d77ac1448cfc21a5d6
Unknown <aguarinbayo@uniminuto.edu.co>
2019-03-18 16:25:35 -0500

    Archivos Iniciales Con base Datos
    Se encuentran los archivos iniciales con base datos como servicios

---
+++ src/java/co/edu/uniminuto/arqSw/unimonito/DAO/queryDAO.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniminuto.arqSw.unimonito.DAO;
+
+import co.edu.uniminuto.arqSW.unimonito.hibernate.HibernateUtil;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author clarylinux
+ */
+
+public class queryDAO {
+    Session session=null;
+    
+    public queryDAO(){
+        this.session=HibernateUtil.getSessionFactory().getCurrentSession();
+    }
+
+}
